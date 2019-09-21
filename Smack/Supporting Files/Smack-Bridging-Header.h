aec2918bea38dae6117e87c922ed479522771bce
farnazed <farnaz.e.daneshvar@gmail.com>
2019-09-21 11:02:55 -0600

    SWReveal files added to project

---
+++ Smack/Supporting Files/Smack-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Smack-Bridging-Header.h
+//  Smack
+//
+//  Created by farnaz on 2019-09-21.
+//  Copyright © 2019 farnaz. All rights reserved.
+//
+
+#ifndef Smack_Bridging_Header_h
+#define Smack_Bridging_Header_h
+
+#import "SWRevealViewController.h"
+
+#endif /* Smack_Bridging_Header_h */
